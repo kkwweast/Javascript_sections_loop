6f09b2bc878511462fb6d38efcfd6d8be2c81e5b
Kim Weast <weastkim3@gmail.com>
2023-10-13 20:22:39 -0500

    JavaScriot_Selections_Loops

---
+++ loops.js
@@ -0,0 +1,15 @@
+
+let base = prompt("what number do you want to a multification table of?")
+prompt
+let range = promt("What number should I stop at?")
+
+if(!base)
+ base = 1;
+}
+
+for(let i = 1; i <= range; i++) {
+      //loop body console.log(`$(base) x ${i} = ${base *i}`);
+}  console.log (` ${base } .x ${i} =${base * i}`);
+for (let i = i; i <= 10; i += 2) {
+    console.log(i);
+}
